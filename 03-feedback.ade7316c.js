06ac2226f03de11b52958b0f04b5b281bec238cd
YuraIvashkiv <113311064+YuraIvashkiv@users.noreply.github.com>
2023-06-11 07:44:47 +0000

    Deploying to gh-pages from @ YuraIvashkiv/goit-js-hw-08@2abf1cafa0af3e4430395eafdae09d95fa153c85 🚀

--- 03-feedback.ade7316c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".feedback-form")},n={};e.form.addEventListener("input",(function(e){var o=e.target,r=o.name,t=o.value;n[r]=t,console.log(n)}))}();
-//# sourceMappingURL=03-feedback.ade7316c.js.map
--- 03-feedback.f153aedf.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form")},o={};e.form.addEventListener("input",(function({target:e}){const{name:n,value:t}=e;o[n]=t,console.log(o)}));
-//# sourceMappingURL=03-feedback.f153aedf.js.map
